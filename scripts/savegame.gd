c75ea287dc64b66a86232e346da6ea547448b37c
Zhi Liao <25388856@qq.com>
2026-06-16 21:24:31 +0800

    tt

--- scripts/savegame.gd
+++ scripts/savegame.gd
@@ -1,2 +1,33 @@
 class_name Savegame
 extends Resource
+
+@export var character_name: String
+@export var is_demo: = -1
+@export var money: = 0
+@export var last_time_played: = 0
+@export var playtime: = 0.0
+@export var equipments: Array[int] = []
+@export var now_day:int = 1
+
+static var current: Savegame = null
+
+const DEFAULT_PATH: = "user://savegames"
+
+
+func end_level(level_playtime: float):
+	playtime += level_playtime
+	save_to_file()
+
+func load():
+	last_time_played = int(Time.get_unix_time_from_system())
+	save_to_file()
+	current = self
+
+func get_file_path():
+	return DEFAULT_PATH + "/" + "wakuwaku" + ".tres"
+	
+func save_to_file():
+	ResourceSaver.save(self, get_file_path())
+
+func delete():
+	DirAccess.remove_absolute(get_file_path())
